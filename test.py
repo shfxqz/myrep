a9a162747c8cd00512f6cfc316335db8113b74ae
shfxqz <shfxqz@126.com>
2020-03-07 20:05:06 +0800

    2020030701

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/03/06 15:50:19
+@Author  :   徐其周
+@Version :   1.0
+@Contact :   shfxqz@126.com
+@WebSite :   
+'''
+# Start typing your code from here
+
+
+print("hello,world")
+
+for i in range(10):
+    print(i)
